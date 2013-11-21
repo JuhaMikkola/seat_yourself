e85f5dad344cba55690c0911e29d219720def467
Juha Mikkola <sales@floorballpro.com>
2013-11-21 15:09:28 -0500

    Adjust sorcery migration for users

--- db/migrate/20131119193741_sorcery_core.rb
+++ db/migrate/20131119193741_sorcery_core.rb
@@ -1,5 +1,6 @@
 class SorceryCore < ActiveRecord::Migration
   def self.up
+    drop_table :users
     create_table :users do |t|
       t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
       t.string :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
